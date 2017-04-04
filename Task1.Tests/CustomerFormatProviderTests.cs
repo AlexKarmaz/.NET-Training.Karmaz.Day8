520989d7aaf86fda2ad940678ecba6ed9f4c3381
AlexKarmaz <karmazct@mail.ru>
2017-04-04 13:55:54 +0300

    Add Task1

---
+++ Task1.Tests/CustomerFormatProviderTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using NUnit.Framework;
+using System.Collections;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    class CustomerFormatProviderTests
+    {
+        private static Customer customer = new Customer("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
+
+        class TestDatas : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new TestCaseData(customer, "{0:A}").Returns($"Customer record: Jeffrey Richter, 1000000,00, +1 (425) 555-0100");
+                yield return new TestCaseData(customer, "{0:C}").Returns($"Customer record: +1 (425) 555-0100");
+                yield return new TestCaseData(customer, "{0:E}").Returns($"Customer record: Jeffrey Richter Revenue: 1000000 Contact phone: +1 (425) 555-0100");
+            }
+        }
+
+        [Test, TestCaseSource(typeof(TestDatas))]
+        public static string Format_PositiveTest(Customer customer, string format)
+        {
+            IFormatProvider fp = new CustomerFormatProvider();
+            return string.Format(fp, format, customer);
+        }
+
+        [Test]
+        public static void Format_ThrowsFormatException()
+        {
+            IFormatProvider fp = new CustomerFormatProvider();
+            Assert.Throws<FormatException>(() => string.Format(fp,"{0:w}", customer));
+        }
+    }
+}
---
+++ Task1.Tests/CustomerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using NUnit.Framework;
+using System.Collections;
+
+namespace Task1.Tests
+{
+    [TestFixture]
+    class CustomerTest
+    {
+        private  static Customer customer = new Customer("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
+
+        class TestDatas : IEnumerable
+        {
+            public IEnumerator GetEnumerator()
+            {
+                yield return new TestCaseData("A", CultureInfo.InvariantCulture).Returns($"Customer record: Jeffrey Richter, 1000000.00, +1 (425) 555-0100");
+                yield return new TestCaseData("C", CultureInfo.InvariantCulture).Returns($"Customer record: +1 (425) 555-0100");
+                yield return new TestCaseData("B", CultureInfo.InvariantCulture).Returns($"Customer record: Jeffrey Richter, 1000000.00");
+                yield return new TestCaseData("N", CultureInfo.InvariantCulture).Returns($"Customer record: Jeffrey Richter");
+                yield return new TestCaseData("R", CultureInfo.InvariantCulture).Returns($"Customer record: 1000000.00");
+                yield return new TestCaseData("D", CultureInfo.InvariantCulture).Returns($"Customer record: Jeffrey Richter, +1 (425) 555-0100");
+                yield return new TestCaseData("", CultureInfo.InvariantCulture).Returns($"Customer record: Jeffrey Richter, 1000000.00, +1 (425) 555-0100");
+
+            }
+        }
+
+        [Test, TestCaseSource(typeof(TestDatas))]
+        public static string ToString_PositiveTest(string format, IFormatProvider formatProvider)
+        {
+            return customer.ToString(format, formatProvider);
+        }
+
+        [Test]
+        public static void ToString_ThrowsFormatException()
+        {
+            Assert.Throws<FormatException>(() => customer.ToString("E", CultureInfo.InvariantCulture));
+        }
+    }
+}
