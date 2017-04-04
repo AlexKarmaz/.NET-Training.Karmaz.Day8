96b273160f3773318ca11633f20b42f2ba849294
AlexKarmaz <karmazct@mail.ru>
2017-04-04 18:17:44 +0300

    Add XML documentation comments

--- Task1/Customer.cs
+++ Task1/Customer.cs
@@ -7,27 +7,61 @@ using System.Threading.Tasks;
 
 namespace Task1
 {
+    /// <summary>
+    /// Contains the information about customer.
+    /// </summary>
     public class Customer : IFormattable
     {
+        #region Properties
         public string Name { get; private set; }
         public string ContactPhone { get; private set; } 
         public decimal Revenue { get; private set; }
+        #endregion
 
+        #region Constructors
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public Customer() : this(string.Empty, string.Empty, default(decimal))
         {
         }
 
+        /// <summary>
+        /// Constructor with parameters of class Customer
+        /// </summary>
+        /// <param name="name">Customer's name</param>
+        /// <param name="contactPhone">Customer's contact phone</param>
+        /// <param name="revenue">Customer's  revenue</param>
         public Customer(string name, string contactPhone, decimal revenue)
         {
             Name = name;
             ContactPhone = contactPhone;
             Revenue = revenue;
         }
+        #endregion
 
+        #region Public methods
+        /// <summary>
+        /// Returns string representation of Customer
+        /// </summary>
+        /// <returns>String representation of Customer</returns>
         public override string ToString() => this.ToString("G", CultureInfo.CurrentCulture);
 
+        /// <summary>
+        /// Returns string representation of Customer
+        /// </summary>
+        /// <param name="format">A format string for Customer object</param>
+        /// <returns>String representation of Customer</returns>
+        /// <exception cref="FormatException">The format string is invalid</exception>
         public string ToString(string format) => this.ToString(format, CultureInfo.CurrentCulture);
 
+        /// <summary>
+        /// Returns string representation of Customer
+        /// </summary>
+        /// <param name="format">A format string for Customer object</param>
+        /// <param name="formatProvider">An object that supplies specific formatting information</param>
+        /// <returns>String representation of Customer</returns>
+        /// <exception cref="FormatException">The format string is invalid</exception>
         public string ToString(string format, IFormatProvider formatProvider)
         {
             if (string.IsNullOrEmpty(format))
@@ -53,7 +87,7 @@ namespace Task1
                 default:
                     throw new FormatException();
             }
-
         }
+        #endregion
     }
 }
--- Task1/CustomerFormatProvider.cs
+++ Task1/CustomerFormatProvider.cs
@@ -7,17 +7,34 @@ using System.Threading.Tasks;
 
 namespace Task1
 {
+    /// <summary>
+    /// Provides additional format for Customer objects
+    /// </summary>
     public class CustomerFormatProvider : IFormatProvider, ICustomFormatter
     {
         private readonly IFormatProvider parentProvider;
 
+        #region Constructors
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public CustomerFormatProvider() : this(CultureInfo.CurrentCulture) { }
 
+        /// <summary>
+        /// Constructor with parameters of class CustomerFormatProvider
+        /// </summary>
         public CustomerFormatProvider(IFormatProvider parent)
         {
             parentProvider = parent;
         }
+        #endregion
 
+        #region Public methods
+        /// <summary>
+        /// Returns an object that provides formatting services for the Customer type
+        /// </summary>
+        /// <param name="formatType">An object that specifies the type of format object to return.</param>
+        /// <returns>Object that provides formatting services for the Customer type</returns>
         public object GetFormat(Type formatType)
         {
             if (formatType == typeof(ICustomFormatter))
@@ -26,6 +43,15 @@ namespace Task1
                 return null;
         }
 
+        /// <summary>
+        /// Converts customer object to an equivalent string representation using specified format and specific formatting information.
+        /// </summary>
+        /// <param name="format">A format string containing formatting specifications</param>
+        /// <param name="arg">Instance of the Customer</param>
+        /// <param name="formatProvider">An object that supplies format information about the current instance.</param>
+        /// <returns>The string representation of Customer object, formatted as specified by format and formatProvider</returns>
+        /// <exception cref="FormatException">The format string is invalid</exception>
+        /// <exception cref="ArgumentException">Object reference doesn't refer to Customer instance</exception>
         public string Format(string format, object arg, IFormatProvider formatProvider)
         {
             Customer customer = arg as Customer;
@@ -48,5 +74,6 @@ namespace Task1
                 }
             }
         }
+        #endregion
     }
 }
